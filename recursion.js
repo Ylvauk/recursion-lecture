797407fb41978761496c511cd2a6e73066db511d
Zoe Peterson <zoe@silverbeach.com>
2022-05-23 09:39:28 -0700

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,10 +1,7 @@
 // Write code inside the functions
 // You will have to figure out what parameters to include
 // All functions must use recursion
-
-function findMax(){
-    // This function returns the largest number in a given array.
-}
+// You may find the string method slice helpful: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
 
 function factorial(n){
     // This function returns the factorial of a given number.
@@ -16,6 +13,26 @@ function fibonacci(){
     // For this function, the first two fibonacci numbers are 1 and 1
 }
 
+function isPalindrome() {
+    // This function returns True if the provided string is a palindrome and returns false if it is not.
+}
+        
+
+function changeXY() {
+    // This function is given a string and returns a new string where all the lowercase 'x' chars have been changed to 'y' chars. No loops!
+    // For example
+    // changeXY("codex") → "codey"
+    // changeXY("xxhixx") → "yyhiyy"
+    // changeXY("xhixhix") → "yhiyhiy"
+}
+
+
+function findMax(){
+    // This function returns the largest number in a given array.
+}
+
+
+// coinFlips is a hungry for more challenging recursion problem! Not required as part of the deliverable.
 function coinFlips(){
     // This function returns an array of all possible outcomes from flipping a coin N times.
     // Input type: Integer
@@ -25,17 +42,12 @@ function coinFlips(){
     // Represent the two outcomes of each flip as "H" or "T"
 }
 
-function letterCombinations(){
-    // This function returns an array of all combinations of the given letters
-    // Input type: Array of single characters
-    // For example, letterCombinations(["a","b","c"]) would return the following:
-    // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
-}
 
 module.exports = {
-    findMax,
     factorial,
     fibonacci,
+    isPalindrome,
+    changeXY,
+    findMax,
     coinFlips,
-    letterCombinations
 }
